00b115c60d603ef589ead5f97d03af3a2f5fbc6a
yooDeve <janhold@naver.com>
2022-08-14 18:26:34 +0900

    third

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter,Route, Routes } from "react-router-dom";
+import Detail from "../routes/Detail";
+import Home from "../routes/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/:id" element={<Detail />} />
+        </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,61 +1,12 @@
-import { createStore } from "redux";
-
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
-
-const ADD_TODO = "ADD_TODO";
-const DELETE_TODO = "DELETE_TODO";
-
-const reducer = (state = [], action) => {
-  console.log(action);
-  switch (action.type) {
-    case ADD_TODO:
-      return [...state, { text: action.text, id : Date.now() }];
-    case DELETE_TODO:
-      return [];
-    default:
-      return state;
-  }
-};
-
-const store = createStore(reducer);
-
-store.subscribe(() => console.log(store.getState()));
-
-const addToDo = (text) => {
-  store.dispatch({type:ADD_TODO, text})
- }
-
-const deleteTodo = (e) => {
-  const id = e.target.parentNode;
-  store.dispatch({type:DELETE_TODO, id})
- }
-
- 
-const paintTodos = () => {
-  const toDos = store.getState();
-  ul.innerHTML="";
-  toDos.forEach(todo => {
-    const li = document.createElement("li");
-    const btn = document.createElement("button");
-    btn.innerText = "DEL";
-    btn.addEventListener("click", deleteTodo)
-    li.id = todo.id;
-    li.innerText = todo.text;
-    li.appendChild(btn);
-    ul.appendChild(li);
-  })
-}
-
-store.subscribe(paintTodos)
-
-
-const onSubmit = (e) => {
-  e.preventDefault();
-  const todo = input.value;
-  input.value = "";
-  addToDo(todo);
-};
-
-form.addEventListener("submit", onSubmit);
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./components/App";
+import store from "./routes/store";
+const container = document.getElementById("root");
+const root = createRoot(container);
+// createRoot(container!) if you use TypeScript
+root.render(
+  <Provider store={store}>
+    <App tab="home" />
+  </Provider>
+);
